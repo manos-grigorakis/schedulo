47b93ef2ac7366033834efe60951f140e21d162d
Manos Grigorakis <manos.grigorakis03@gmail.com>
2026-09-10 15:11:26 +0300

    Chore/setup project (#2)
    * chore: setup backend project structure along with Docker compose for DB
    
    * misc: add root directory gitignore
    
    * chore: setup frontend project

---
+++ backend/src/test/java/com/mgrigorakis/schedulo/ScheduloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgrigorakis.schedulo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
