c430e7cae0c375146aae187d1ba6191b28f8e4fb
Manos Grigorakis <manos.grigorakis03@gmail.com>
2026-09-10 14:54:29 +0300

    chore: setup backend project structure along with Docker compose for DB

---
+++ backend/src/main/java/com/mgrigorakis/schedulo/ScheduloApplication.java
@@ -0,0 +1,13 @@
+package com.mgrigorakis.schedulo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduloApplication.class, args);
+    }
+
+}
